7f086a964961d2dd87b82b85a859c205e7c717b4
David Lisuk <david@lisuk.dev>
2023-04-14 17:14:42 -0400

    No need to pad minutes

--- components/TimerDisplay.js
+++ components/TimerDisplay.js
@@ -2,6 +2,6 @@ import {Text} from "react-native";
 
 export default function TimerDisplay({seconds, style}) {
     return <Text style={style}>
-        {String(Math.floor(seconds / 60)).padStart(2, " ")}:{String(seconds % 60).padStart(2, "0")}
+        {Math.floor(seconds / 60)}:{String(seconds % 60).padStart(2, "0")}
     </Text>
 }
